3fd1d16e185c2386ead46c7826cc9f8b2faa815d
Henry <henrykc24@gmail.com>
2022-11-30 22:00:43 +0100

    Add navigation to the app

--- App.js
+++ App.js
@@ -1,7 +1,47 @@
-import Home from "./pages/Home";
-import SignIn from "./pages/SignIn";
-import Birthday18 from "./pages/Birthday18";
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
 
-export default function App() {
-  return <Birthday18 />;
-}
+import * as React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
+
+import { NavigationContainer } from "@react-navigation/native";
+import store from "./redux/configureStore";
+import { Provider } from "react-redux";
+import Birthday18 from "./pages/Birthday18";
+import SignIn from "./pages/SignIn";
+import Welcome from "./pages/Welcome";
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Welcome"
+            component={Welcome}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Birthday18"
+            component={Birthday18}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="SignIn"
+            component={SignIn}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+  );
+};
+
+export default App;
