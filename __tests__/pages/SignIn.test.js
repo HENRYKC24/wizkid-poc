d56c2995bf7945d49e8daa1999414e72f1e0b728
Henry <henrykc24@gmail.com>
2022-12-08 21:21:57 +0100

    Write test for the SignIn page

---
+++ __tests__/pages/SignIn.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+jest.useFakeTimers()
+import renderer from "react-test-renderer";
+
+import SignIn from "../../pages/SignIn";
+
+describe("<SignIn />", () => {
+  it("has 1 child", () => {
+    const tree = renderer.create(<SignIn />).toJSON();
+    expect(tree.children.length).toBe(5);
+  });
+
+  it("renders correctly", () => {
+    const tree = renderer.create(<SignIn />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
