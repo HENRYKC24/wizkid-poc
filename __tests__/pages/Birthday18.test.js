6d5becab88c9c5c12fa3657926e1cdbf66225129
Henry <henrykc24@gmail.com>
2022-12-08 21:22:32 +0100

    Write test for the Birthday18 page

---
+++ __tests__/pages/Birthday18.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import renderer from "react-test-renderer";
+
+import Birthday18 from "../../pages/Birthday18";
+
+describe("<Birthday18 />", () => {
+  it("has 1 child", () => {
+    const tree = renderer.create(<Birthday18 />).toJSON();
+    expect(tree.length).toBe(2);
+  });
+
+  it("renders correctly", () => {
+    const tree = renderer.create(<Birthday18 />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
